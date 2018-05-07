318bdd98582b54f2674eb005b87c6502790d3ef2
NathanHigley <nathan@jhigley.com>
2018-05-07 16:15:50 -0700

    3.4

--- js/script.js
+++ js/script.js
@@ -3,13 +3,18 @@ var titles = new Array(
   'When in Rome, Sink Ships',
   'TUMS&#174 Festival',
   'Another Happy Landing',
+  'Nice Talkin\' To Ya',
+  'With No Survivors',
   'The Fire Rises',
-  'You don\'t get to bring friends',
-  'He didn\'t fly so good',);
+  'You Don\'t Get To Bring Friends',
+  'He Didn\'t Fly So Good',);
 var title = titles[parseInt(Math.random() * titles.length)];
 //SUBTITLE
 var subtitles = new Array(
     'Icon of the Hubris',
     'Hambone Extraordinaire',
-    'Wiggley Dude');
+    'O b',
+    'Dude Man Dude',
+    'Paralyzer of the Pancreas',
+    'The Guy That Isn\'t The Other Guy');
 var subtitle = subtitles[parseInt(Math.random() * subtitles.length)];
