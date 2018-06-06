2e6f8ffa128f432a021c6d159e45ddc84c82041d
NathanHigley <nathan@jhigley.com>
2018-06-06 15:32:02 -0700

    FIXED

---
+++ archive/laika/script.js
@@ -0,0 +1,17 @@
+function openNav(){
+  document.getElementById("mainnav").style.width = "250px";
+}
+
+function closeNav(){
+  document.getElementById("mainnav").style.width = "0";
+}
+
+function swap(page){
+   var id = page.dataset.id;
+   var list = document.querySelectorAll(".trans");
+
+   list.forEach(function(item) {
+      item.classList.remove("trans");
+   })
+   document.getElementById(id).classList.add("trans");
+}
